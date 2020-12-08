ab4904ee04cba441a9f560aec8d33b862fd09deb
Joshua Cantero <cantero.joshua@gmail.com>
2020-12-08 15:52:25 -0800

    react states

---
+++ src/StateExample.jsx
@@ -0,0 +1,89 @@
+import React, { useState, Component } from 'react'
+
+// function FunctionalComponent() {
+//     const [name, setName] = useState('');
+
+//     const handleOnNameChanged = (e) => {
+//         // We update the state "name" with the new value that's in the input field
+//         setName(e.target.value);
+//     }    
+
+//     return (
+//         <div>
+//             {/* 
+//                 Here we render the name that gets changed every time "setName" is called
+//             */}
+//             <p>{name}</p>
+            
+//             {/* 
+//                 input html tags have an onChange "callback" function
+//                 that is fired every time a change happens in the input field.
+
+//                 When we edit the input field here, we call the handleOnNameChanged function defined 
+//                 in this function.... yeah, functions defined in functions. Seems kind of 
+//                 weird at first...
+//             */}
+//             <input onChange={handleOnNameChanged}/>
+//         </div>
+//     );
+// }
+
+
+// In react, class base components need to always extend Component.
+class FunctionalComponent extends Component {
+    
+    constructor() {
+        super();
+        this.state = {
+            name: ''
+        }
+    }
+
+
+    handleOnNameChanged = (e) => {
+        this.setState({name: e.target.value});
+    }
+
+    // This method is provided to us by the Component super class
+    // In the render method we always want to return jsx or null.
+    /* 
+        When returning jsx, we need to always only send a single node... for example
+
+        GOOD: 
+            return (
+                <div>
+                    ....
+                </div>
+            );
+        BAD:
+            return (
+                <div>
+                </div>
+                <div>
+                </div>
+            );
+    */
+    render() {
+        
+        return (
+            <div>
+                {/* 
+                    Here we render the name that gets changed every time "setName" is called
+                */}
+                <p>{this.state.name}</p>
+                
+                {/* 
+                    input html tags have an onChange "callback" function
+                    that is fired every time a change happens in the input field.
+
+                    When we edit the input field here, we call the handleOnNameChanged function defined 
+                    in this function.... yeah, functions defined in functions. Seems kind of 
+                    weird at first...
+                */}
+                <input onChange={this.handleOnNameChanged}/>
+            </div>
+        );
+    }
+}
+
+export default FunctionalComponent;
